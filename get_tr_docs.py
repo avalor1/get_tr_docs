3d79b83eb3c932688d306d18c862a23d77b38643
avalor <avalor_dds@web.de>
2025-10-22 14:47:43 +0200

    pylance formating

--- get_tr_docs.py
+++ get_tr_docs.py
@@ -1,14 +1,14 @@
 #! /bin/python
-'''
-This script uses the pytr script (https://github.com/pytr-org/pytr) to download all transaction 
-documents from Trade Republic via API, creates a CSV for import into Portfolio Performance 
+"""
+This script uses the pytr script (https://github.com/pytr-org/pytr) to download all transaction
+documents from Trade Republic via API, creates a CSV for import into Portfolio Performance
 and Uploads all of them them into the corresponding folder of your nextcloud instance.
 It is configured via a .env file and optional parameters.
 
 Usage: get_tr_docs.py [--help] [--nodl] [--skipdel] [--nocsv] [--noupload]
 
 Author: Andreas Hering, 2025
-'''
+"""
 
 import argparse
 import subprocess
@@ -26,7 +26,7 @@ load_dotenv()
 # see .env.example for keys
 tr_phone_number = os.getenv("TR_PHONE_NUMBER")
 tr_pin = os.getenv("TR_PIN")
-tr_days_to_download = os.getenv("TR_DAYS_TO_DOWNLOAD") # use 0 for all
+tr_days_to_download = os.getenv("TR_DAYS_TO_DOWNLOAD")  # use 0 for all
 tr_doc_download_path = os.getenv("TR_DOC_DOWNLOAD_PATH")
 nc_auth_user = os.getenv("NC_AUTH_USER")
 nc_auth_pass = os.getenv("NC_AUTH_PASS")
@@ -35,17 +35,39 @@ nc_url = os.getenv("NC_URL")
 ###
 
 # Add arguments for script control
-parser = argparse.ArgumentParser(description="Download Trade Republic docs, generate CSV for importing into Portfolio Performance and upload docs to Nextcloud")
-parser.add_argument("--nodl", "--skip-doc-download", help="Skip document download from Trade Republic", action="store_true")
-parser.add_argument("--skipdel", "--skip-dl-folder-deletion", help="Skip deletion of existing local download folder", action="store_true")
-parser.add_argument("--nocsv", "--skip-csv-generation", help="Skip generation of CSV for import into Portfolio Performance", action="store_true")
-parser.add_argument("--noupload", "--skip-nextcloud-upload", help="Skip folder creation in and upload of files to Nextcloud.", action="store_true")
+parser = argparse.ArgumentParser(
+    description="Download Trade Republic docs, generate CSV for importing into Portfolio Performance and upload docs to Nextcloud"
+)
+parser.add_argument(
+    "--nodl",
+    "--skip-doc-download",
+    help="Skip document download from Trade Republic",
+    action="store_true",
+)
+parser.add_argument(
+    "--skipdel",
+    "--skip-dl-folder-deletion",
+    help="Skip deletion of existing local download folder",
+    action="store_true",
+)
+parser.add_argument(
+    "--nocsv",
+    "--skip-csv-generation",
+    help="Skip generation of CSV for import into Portfolio Performance",
+    action="store_true",
+)
+parser.add_argument(
+    "--noupload",
+    "--skip-nextcloud-upload",
+    help="Skip folder creation in and upload of files to Nextcloud.",
+    action="store_true",
+)
 args = parser.parse_args()
 
 
-# Ensure that local download folder is empty when starting, to only create CSV for relevant files 
-# and upload needed files with less duplicates. 
-# (Nextcloud api does not check for existing files and just overwrites existing ones thus we try 
+# Ensure that local download folder is empty when starting, to only create CSV for relevant files
+# and upload needed files with less duplicates.
+# (Nextcloud api does not check for existing files and just overwrites existing ones thus we try
 # to reduce overhead of already uploaded files)
 def remove_existing_dl_folder():
     print("Checking for existing download folder.")
@@ -53,13 +75,23 @@ def remove_existing_dl_folder():
         if len(os.listdir(tr_doc_download_path)) > 0:
             shutil.rmtree(tr_doc_download_path)
             print(f"Deleted existing & not empty doc path: '{tr_doc_download_path}'")
-        else:
-            print("All good, no folder found. Starting download.")
+    else:
+        print("All good, no folder found. Starting download.")
 
 
 # Download documents from Trade Republic
 def download_docs():
-    pytr_dl_docs_args = (["pytr", "dl_docs", "-n", tr_phone_number, "-p", tr_pin, "--last_days", tr_days_to_download, tr_doc_download_path])
+    pytr_dl_docs_args = [
+        "pytr",
+        "dl_docs",
+        "-n",
+        tr_phone_number,
+        "-p",
+        tr_pin,
+        "--last_days",
+        tr_days_to_download,
+        tr_doc_download_path,
+    ]
     dl_docs = subprocess.Popen(
         pytr_dl_docs_args,
         stdin=subprocess.PIPE,
@@ -68,7 +100,7 @@ def download_docs():
         encoding="utf-8",
         text=True,  # Use text mode for input/output
         bufsize=1,  # Line-buffered for reading output line by line
-        shell=True  # Use the shell to execute the command
+        shell=True,  # Use the shell to execute the command
     )
     # Read line from command line input (the code)
     user_code = input("Enter verification code: ")
@@ -92,7 +124,12 @@ def create_pp_csv():
     timestr = time.strftime("%Y%m%d")
     pp_csv_name = f"{timestr}_pp_import.csv"
     pp_csv_path = os.path.join(tr_doc_download_path, pp_csv_name)
-    pytr_pp_csv_args = (["pytr", "export_transactions", pp_csv_source_events, pp_csv_path])
+    pytr_pp_csv_args = [
+        "pytr",
+        "export_transactions",
+        pp_csv_source_events,
+        pp_csv_path,
+    ]
     pp_csv_gen = subprocess.Popen(
         pytr_pp_csv_args,
         stdin=subprocess.PIPE,
@@ -100,9 +137,9 @@ def create_pp_csv():
         stdout=subprocess.PIPE,
         text=True,  # Use text mode for input/output
         bufsize=1,  # Line-buffered for reading output line by line
-        shell=True  # Use the shell to execute the command
+        shell=True,  # Use the shell to execute the command
     )
-    # wait for popen to complete and write the data (asynchronous execution). 
+    # wait for popen to complete and write the data (asynchronous execution).
     # Also save returned info into variables for eventual debug.
     stdout, stderr = pp_csv_gen.communicate()
     print("CSV generation process exited with return code:", pp_csv_gen.returncode)
@@ -113,7 +150,9 @@ def create_pp_csv():
 
 # create nextcloud connection for folder creation and file upload
 def create_nextcloud_connection():
-    nc_connection = Nextcloud(nextcloud_url=nc_url, nc_auth_user=nc_auth_user, nc_auth_pass=nc_auth_pass)
+    nc_connection = Nextcloud(
+        nextcloud_url=nc_url, nc_auth_user=nc_auth_user, nc_auth_pass=nc_auth_pass
+    )
     return nc_connection
 
 
@@ -122,18 +161,20 @@ def create_nextcloud_connection():
 def create_nextcloud_folders():
     nc = create_nextcloud_connection()
     # Get all nextcloud directories recursively
-    search_result = nc.files.find(["eq", "name", nc_tr_document_folder ])
+    search_result = nc.files.find(["eq", "name", nc_tr_document_folder])
     ## debug
     # print(search_result)
     if not search_result:
         if nc_tr_document_folder not in search_result:
             print("Creating upload target folders!")
-            directories = [x for x in Path(tr_doc_download_path).rglob("*") if x.is_dir()]
+            directories = [
+                x for x in Path(tr_doc_download_path).rglob("*") if x.is_dir()
+            ]
             for directory in directories:
                 print(f"Creating: {directory}")
                 nc.files.makedirs(str(directory), exist_ok=True)
     else:
-        print("Skip folder creation! Already existing!")    
+        print("Skip folder creation! Already existing!")
 
     ## debug
     # print("Files & folders on the instance for the selected user:")
@@ -141,6 +182,7 @@ def create_nextcloud_folders():
     # for obj in tr_documents:
     #     print(obj.user_path)
     print("Folder creation successful!")
+
 
 # Upload files into corresponding folders
 def upload_docs_to_nextcloud():
@@ -154,7 +196,9 @@ def upload_docs_to_nextcloud():
             local_file_path = os.path.join(root, file_name)
             # Construct the relative path for remote upload preserving folder structure
             relative_path = os.path.relpath(local_file_path, tr_doc_download_path)
-            remote_file_path = os.path.join(nc_tr_document_folder, relative_path).replace("\\", "/")
+            remote_file_path = os.path.join(
+                nc_tr_document_folder, relative_path
+            ).replace("\\", "/")
 
             with open(local_file_path, "rb") as f:
                 print(f"Uploading file {f}")
@@ -164,7 +208,7 @@ def upload_docs_to_nextcloud():
 
 
 # bring it all together and do some work
-if not args.skipdel: 
+if not args.skipdel:
     remove_existing_dl_folder()
 
 if not args.nodl:
